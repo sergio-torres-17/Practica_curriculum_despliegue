72cd4973dec827b625d59cf81c5a68aa0005ce6f
Sergio Saúl Torres Ibarra <50086305+sergio-torres-17@users.noreply.github.com>
2023-09-17 16:57:49 -0600

    correción ortogra´fica

--- Db/2. Insert Base data.sql
+++ Db/2. Insert Base data.sql
@@ -9,10 +9,10 @@ INSERT INTO `Programas` (`NombrePrograma`,`NivelDominio`)VALUES('Word',5);
 INSERT INTO `Programas` (`NombrePrograma`,`NivelDominio`)VALUES('Power Point',4);
 INSERT INTO `Programas` (`NombrePrograma`,`NivelDominio`)VALUES('Publisher',2);
 -- Experiencia Laboral 
-INSERT INTO `experiencialaboral` (`PersonaID`,`Puesto`,`Empresa`,`FechaInicio`,`FechaFin`,`Descripcion`) VALUES (1,'Profesor de robotica','Instituto pierre faure',cast('2020-02-05' as datetime),cast('2020-06-15' as datetime),'Impartía la materia de fundamentos de robotica para grados de secundaria.');
-INSERT INTO `experiencialaboral` (`PersonaID`,`Puesto`,`Empresa`,`FechaInicio`,`FechaFin`,`Descripcion`) VALUES (1,'Desarrollador Backend','Multi-Code MX Systems',cast('2016-02-15' as datetime),cast(now() as datetime),'Desarrollo general de proyectos a clientes independientes');
-INSERT INTO `experiencialaboral` (`PersonaID`,`Puesto`,`Empresa`,`FechaInicio`,`FechaFin`,`Descripcion`) VALUES (1,'Desarrollador COBOL','ABC Consultora',cast('2019-08-05' as datetime),cast('2020-10-03' as datetime),'Desarrollo de mdulos COBOL para industria bancaria');
-INSERT INTO `experiencialaboral` (`PersonaID`,`Puesto`,`Empresa`,`FechaInicio`,`FechaFin`,`Descripcion`) VALUES (1,'Analista de soporte de tiendas','Grupo Charly',cast('2020-09-15' as datetime),cast(now() as datetime),'Analista de soporte de tiendas');
+INSERT INTO `ExperienciaLaboral` (`PersonaID`,`Puesto`,`Empresa`,`FechaInicio`,`FechaFin`,`Descripcion`) VALUES (1,'Profesor de robotica','Instituto pierre faure',cast('2020-02-05' as datetime),cast('2020-06-15' as datetime),'Impartía la materia de fundamentos de robotica para grados de secundaria.');
+INSERT INTO `ExperienciaLaboral` (`PersonaID`,`Puesto`,`Empresa`,`FechaInicio`,`FechaFin`,`Descripcion`) VALUES (1,'Desarrollador Backend','Multi-Code MX Systems',cast('2016-02-15' as datetime),cast(now() as datetime),'Desarrollo general de proyectos a clientes independientes');
+INSERT INTO `ExperienciaLaboral` (`PersonaID`,`Puesto`,`Empresa`,`FechaInicio`,`FechaFin`,`Descripcion`) VALUES (1,'Desarrollador COBOL','ABC Consultora',cast('2019-08-05' as datetime),cast('2020-10-03' as datetime),'Desarrollo de mdulos COBOL para industria bancaria');
+INSERT INTO `ExperienciaLaboral` (`PersonaID`,`Puesto`,`Empresa`,`FechaInicio`,`FechaFin`,`Descripcion`) VALUES (1,'Analista de soporte de tiendas','Grupo Charly',cast('2020-09-15' as datetime),cast(now() as datetime),'Analista de soporte de tiendas');
 -- Programas persona
 INSERT INTO `ProgramasPersona`(`PersonaID`,`ProgramaID`,`NivelDominio`)VALUES(1,1,5);
 INSERT INTO `ProgramasPersona`(`PersonaID`,`ProgramaID`,`NivelDominio`)VALUES(1,2,5);
